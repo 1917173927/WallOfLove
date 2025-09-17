622156bfd2b6d3c67c5b42827d6ffa54bc0f3e20
黑蚊子多 <3511457481@qq.com>
2025-09-17 18:33:19 +0800

    post
    feat: 更新帖子模型，添加用户昵称和头像ID；优化创建和更新帖子逻辑

--- app/controllers/postController.go
+++ app/controllers/postController.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"errors"
-	"log"
-
 	"github.com/1917173927/WallOfLove/app/models"
 	"github.com/1917173927/WallOfLove/app/services"
 	"github.com/1917173927/WallOfLove/app/utils"
@@ -20,26 +18,27 @@ type PostData struct{
 func CreatePost(c *gin.Context) {
 	var req PostData
 	uid, _ := c.Get("userID")
-	UID := uid.(uint64)   //jwt
+	UID := uid.(uint64)   
 	if err := c.ShouldBindJSON(&req); err != nil {
 		utils.JsonErrorResponse(c, 400, "参数错误")
 		return
 	}
-
-	if UID == 0 {
-		utils.JsonErrorResponse(c, 400, "用户ID不能为空")
+	user,err:=services.GetUserDataByID(UID)
+	if err!=nil{
+		utils.JsonErrorResponse(c, 400, "用户不存在")
 		return
 	}
-	
 	if req.Content == "" {
 		utils.JsonErrorResponse(c, 400, "表白内容不能为空")
 		return
 	}
 	if err := services.CreatePost(&models.Post{
-		UserID:    UID,
-		Content:   req.Content,
-		Anonymous: req.Anonymous,
+		UserID:     UID,
+		Content:    req.Content,
+		Anonymous:  req.Anonymous,
 		Visibility: req.Visibility,
+		UserNickname: user.Nickname,
+		AvatarImageID: user.AvatarImageID,
 	}); err != nil {
 		utils.JsonErrorResponse(c, 500, "创建帖子失败")
 		return
@@ -98,6 +97,8 @@ func UpdatePost(c *gin.Context) {
 		Anonymous:     anonymous,
 		Visibility:    visibility,	
 		Version:       post.Version,
+		UserNickname:  post.UserNickname,
+		AvatarImageID: post.AvatarImageID,
 	}
 
 	err = services.UpdatePost(&updatedPost, post.Version)
@@ -112,7 +113,7 @@ func UpdatePost(c *gin.Context) {
 
 	c.JSON(200, map[string]any{"version": post.Version+1})
 }
-
+// 删除帖子
 type DeletePostData struct {
 	ID         uint64  `json:"post_id" binding:"required"`
 }
@@ -143,14 +144,27 @@ func DeletePost(c *gin.Context) {
 }
 
 // GetVisiblePosts 获取未被拉黑的其他人发布的表白
+type PageData struct{
+	PageSize int `json:"page_size"`
+	PageNum int `json:"page_num"`
+}
 func GetVisiblePosts(c *gin.Context) {
 	uid, _ := c.Get("userID")
 	UID := uid.(uint64)
-	posts,total,err := services.GetVisiblePosts(UID,10,10)
+	var req PageData
+	err := c.ShouldBindJSON(&req)
+	if err != nil {
+		utils.JsonErrorResponse(c,501, "参数错误")
+		return
+	}
+	posts,total,err := services.GetVisiblePosts(UID,req.PageNum,req.PageSize)
 	if err != nil {
 		utils.JsonErrorResponse(c, 500, "获取帖子失败")
 		return
 	}
-	log.Println(total)
-	utils.JsonSuccessResponse(c, posts)
+	data:=map[string]any{
+		"posts": posts,
+		"total": total,
+	}
+	utils.JsonSuccessResponse(c, data)
 }
--- app/models/post.go
+++ app/models/post.go
@@ -8,7 +8,9 @@ type Post struct {
     Content      string     `json:"content"`
     Anonymous    bool       `json:"anonymous"`
     Visibility   bool       `json:"visibility"`
+    UserNickname string     `json:"user_nickname"`
+    AvatarImageID *uint64     `json:"avatar_image_id"`
     CreatedAt    time.Time  `json:"created_at"`
     UpdatedAt    time.Time  `json:"updated_at"`
-    Version        uint       `grom:"default:1" json:"version"`
+    Version      uint       `grom:"default:1" json:"version"`
 }

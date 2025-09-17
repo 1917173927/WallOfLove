f41be8ef4541b30be17b9641ea5e40d919825496
黑蚊子多 <3511457481@qq.com>
2025-09-17 15:35:16 +0800

    简化分页
    refactor: 移除未使用的分页参数获取功能，简化分页逻辑

--- app/utils/page.go
+++ app/utils/page.go
@@ -1,24 +1,8 @@
 package utils
 
 import (
-	"strconv"
-	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
-
-
-type Page struct {
-	Page     int `json:"page"`     // 当前页
-	PageSize int `json:"pageSize"` // 页大小
-	Total    int `json:"total"`    // 数据量
-}
-
-// GetPaginationParams 获取分页参数
-func GetPaginationParams(c *gin.Context) (page, pageSize int) {
-	page, _ = strconv.Atoi(c.DefaultQuery("page", "1"))
-	pageSize, _ = strconv.Atoi(c.DefaultQuery("pageSize", "10"))
-	return page, pageSize
-}
 
 // Paginate 分页
 func Paginate(page int, size int) func(db *gorm.DB) *gorm.DB {

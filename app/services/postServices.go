943efa160e23694d22e559ca12d95f92dfb1fbcb
吃不饱的幽幽子 <1917173927@qq.com>
2025-09-17 12:25:28 +0800

    refactor: 重构图片上传和帖子控制器，使用JWT获取用户ID并简化服务层调用

--- app/services/postServices.go
+++ app/services/postServices.go
@@ -5,23 +5,22 @@ import (
 	"github.com/1917173927/WallOfLove/conf/database"
 )
 
-type PostService struct{}
 
 // CreatePost 创建帖子，可以独立发布（无需关联图片）
-func (s *PostService) CreatePost(post *models.Post) error {
+func CreatePost(post *models.Post) error {
 	return database.DB.Create(post).Error
 }
 
-func (s *PostService) UpdatePost(post *models.Post) error {
+func UpdatePost(post *models.Post) error {
 	return database.DB.Model(post).Updates(post).Error
 }
 
-func (s *PostService) DeletePost(postID string) error {
+func DeletePost(postID string) error {
 	return database.DB.Delete(&models.Post{}, "id = ?", postID).Error
 }
 
 // GetVisiblePosts 获取未被拉黑的其他人发布的表白
-func (s *PostService) GetVisiblePosts(userID uint64) ([]models.Post, error) {
+func GetVisiblePosts(userID uint64) ([]models.Post, error) {
 	var posts []models.Post
 	err := database.DB.Where("user_id != ? AND id NOT IN (SELECT blocked_id FROM blacklists WHERE user_id = ?)", userID, userID).Find(&posts).Error
 	if err != nil {
